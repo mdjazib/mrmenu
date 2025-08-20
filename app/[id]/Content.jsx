ab94826f262e2379363b1f167460b44e0cc799df
Muhammad Jazib <mjxdex@gmail.com>
2025-08-20 23:00:28 +0500

    Refactor Content component to conditionally render deals section based on availability; improve item display logic and fix in-stock indicator for better clarity.

--- app/[id]/Content.jsx
+++ app/[id]/Content.jsx
@@ -45,42 +45,45 @@ const Content = () => {
                         </header>
                         <div className={sass.menu}>
                             <div className={`${sass.page} ${sass.main}`}>
-                                <div className={`${sass.category} ${open === "deal" && sass.open}`}>
-                                    <div className={sass.header} onClick={() => { setOpen("deal" === open ? "" : "deal") }}>
-                                        <h3>Deals</h3>
-                                        {open === "deal" ? <Minus /> : <Plus />}
-                                    </div>
-                                    <div className={sass.items}>
-                                        <div className={sass.deals}>
-                                            {
-                                                mrmenu.deals.map((deal, i) => (
-                                                    < div key={i} className={sass.deal}>
-                                                        <div className={sass.iheader}>
-                                                            <h2>{deal.name}</h2>
-                                                            <div className={sass.col}>
-                                                                <h5>{deal.price} Rs</h5>
-                                                                <p>Save Rs. {deal.save}</p>
+                                {
+                                    mrmenu.deals.length ?
+                                        <div className={`${sass.category} ${open === "deal" && sass.open}`}>
+                                            <div className={sass.header} onClick={() => { setOpen("deal" === open ? "" : "deal") }}>
+                                                <h3>Deals</h3>
+                                                {open === "deal" ? <Minus /> : <Plus />}
+                                            </div>
+                                            <div className={sass.items}>
+                                                <div className={sass.deals}>
+                                                    {
+                                                        mrmenu.deals.map((deal, i) => (
+                                                            < div key={i} className={sass.deal}>
+                                                                <div className={sass.iheader}>
+                                                                    <h2>{deal.name}</h2>
+                                                                    <div className={sass.col}>
+                                                                        <h5>{deal.price} Rs</h5>
+                                                                        <p>Save Rs. {deal.save}</p>
+                                                                    </div>
+                                                                </div>
+                                                                <table>
+                                                                    <tbody>
+                                                                        {
+                                                                            deal.items.map((item, j) => (
+                                                                                <tr key={j}>
+                                                                                    <td>{item.name}</td>
+                                                                                    <td>{item.quantity}</td>
+                                                                                    <td>{item.price} Rs</td>
+                                                                                </tr>
+                                                                            ))
+                                                                        }
+                                                                    </tbody>
+                                                                </table>
                                                             </div>
-                                                        </div>
-                                                        <table>
-                                                            <tbody>
-                                                                {
-                                                                    deal.items.map((item, j) => (
-                                                                        <tr key={j}>
-                                                                            <td>{item.name}</td>
-                                                                            <td>{item.quantity}</td>
-                                                                            <td>{item.price} Rs</td>
-                                                                        </tr>
-                                                                    ))
-                                                                }
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                ))
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
+                                                        ))
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div> : <></>
+                                }
                                 {
                                     mrmenu.items.map((category, i) => (
                                         <div key={i} className={`${sass.category} ${open === category.category && sass.open}`}>
@@ -93,7 +96,7 @@ const Content = () => {
                                                     {
                                                         category.items.map((item, j) => (
                                                             <tr key={j} className={sass.row}>
-                                                                <td className={sass.name}><div className={sass.flex}>{item.inStock && <X className={sass.out} />}<span>{item.name}</span></div></td>
+                                                                <td className={sass.name}><div className={sass.flex}>{!item.inStock && <X className={sass.out} />}<span>{item.name}</span></div></td>
                                                                 <td className={sass.price}>
                                                                     <div className={sass.flex}>
                                                                         {typeof item.price === "number" ? item.price :
